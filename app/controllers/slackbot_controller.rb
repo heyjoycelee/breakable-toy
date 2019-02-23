04951102186fa4bcedd2a1877e0fa63b5e60181b
Joyce Lee <lee.joyce@live.ca>
2019-02-23 02:10:55 -0500

    Implemented 'about' slash command

--- app/controllers/slackbot_controller.rb
+++ app/controllers/slackbot_controller.rb
@@ -1,13 +1,20 @@
-#require 'slack-ruby-client'
-require 'json'
-
 class SlackbotController < ApplicationController
+
   def enable_event_challenge
     if params[:type] == 'url_verification'
         render json: {'challenge': params[:challenge] }.to_json
     end
   end
 
+  def about
+    render json: {"text": "Hello! I'm Joyce Bot. I'm here to have fun and pet your cats! Here are some commands I know: "}.to_json
+  end
+
   def expense
+  end
+
+  private
+  def valid_slack_token?
+    params[:token] == ENV["SLACK_SIGNING_SECRET"]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post 'slackbot/enable_event_challenge' => 'slackbot#enable_event_challenge'
+  post 'slackbot/about' => 'slackbot#about'
   get 'slackbot/expense'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/message' => 'toys#message'
